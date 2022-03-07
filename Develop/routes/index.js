8eb3c82de9f6a371fa9584429765562c4489634e
unknown <jasonsburke90@gmail.com>
2022-03-06 18:33:00 -0600

    added post route

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,14 +1,30 @@
-const fb = require("express").Router();
-// const { v4: uuidv4 } = require("uuid");
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
-// get route to display notes on the side (should return an array of objects)
-// GET Route for retrieving all the feedback
-fb.get("/", (req, res) =>
+// GET route to display notes on the side (should return an array of objects)
+notes.get("/", (req, res) =>
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 );
-// create a note (post route)
+//POST Route create a note
+notes.post("/", (req, res) => {
+  console.log(req.body);
 
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      tip_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added!`);
+  } else {
+    res.error("Error adding note");
+  }
+});
 // EXTRA credit, delete notes
 
-module.exports = fb;
+module.exports = notes;
